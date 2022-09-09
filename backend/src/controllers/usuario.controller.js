5ef3a9ffce836914405ffdbbaac27701514a8ce7
AlexOscar <alejandro.garcia.carmona@gmail.com>
2022-09-09 05:05:58 +0200

    design register

--- backend/src/controllers/usuario.controller.js
+++ backend/src/controllers/usuario.controller.js
@@ -1,10 +1,12 @@
-import User from "../models/usuario.model.js"
-
+import User from "../models/usuario.model.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const userCtrl = {}
 
 userCtrl.register=async (req,res)=>{
     try{
         const data = req.body;
+        //verificar que el correo no exista
         const existUser = await User.findOne({correo:data.correo});
         if (existUser) {
             return res.status(400).json({
@@ -12,11 +14,16 @@ userCtrl.register=async (req,res)=>{
                 message: "El correo ya existe"
             })
         }
+        //encriptar contraseña
+        data.password = await bcrypt.hash(data.password, 10);
         const newUser = await User.create(data);
+        //crear token
+        const token = jwt.sign({_id:newUser._id}, "secreta");
         res.status(201).json({
             ok:true,
             message: "Usuario creado correctamente",
-            data: newUser,
+            data: {...newUser._doc, token},
+          
         })
     } catch (error) {
         res.status(500).json({
@@ -25,5 +32,24 @@ userCtrl.register=async (req,res)=>{
         });
     }
 };
+
+userCtrl.login = async (req, res) => {
+    try {
+        const data = req.body
+        const existUser = await User.findOne({correo: data.correo});
+        if (!existUser) {
+            return res.status(400).json({
+                ok: false,
+                message: error.message,
+            })
+        }
+        ///VOY POR AQUIIIIIIIIIIIIIIIIIIII 
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            message: error.message,
+        });
+    }
+}
 
 export default userCtrl;
